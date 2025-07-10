8e8cb331d39967d567d2c9bf093f914ac582a0b4
ankita <ankita>
2025-07-11 03:49:40 +0530

    Fix case-sensitive import paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Sidebar from './components/Sidebar/sidebar'
+import Sidebar from './components/Sidebar/Sidebar'
 import Main from './components/Main/Main'
 import 'devextreme/dist/css/dx.light.css';
 
